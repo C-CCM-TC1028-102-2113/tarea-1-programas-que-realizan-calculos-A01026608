899be8486a8127edf69baee9c579620f42858a9d
A01026608 <89270787+A01026608@users.noreply.github.com>
2021-08-22 16:04:01 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,6 +1,15 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    minu= float(input('Dame los minutos:'))
+    sec= 60
+    tiempo= minu*sec
 
+    car= 5.7
+    reco= car*tiempo
+
+    conve= reco /10
+
+    print('Centímentros recorridos:', conve)
+    
 if __name__ == '__main__':
     main()
